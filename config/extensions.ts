aaffeb8edc08cc64f71baddeba4d06344b0c2ac5
is0692vs <is0692vs@ed.ritsumei.ac.jp>
2025-10-26 15:12:30 +0900

    🧩 VSCode拡張監視リストに hirokimukai.code-mantra を追加、配列に型注釈を明示

--- config/extensions.ts
+++ config/extensions.ts
@@ -1,4 +1,4 @@
 // config/extensions.ts
 // VSCode拡張機能の監視リスト
 // 形式: publisher.extension-name
-export const vscodeExtensions = ["hirokimukai.jules-extension"];
+export const vscodeExtensions: string[] = ["hirokimukai.jules-extension", "hirokimukai.code-mantra"];
